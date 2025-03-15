68aa1f3f1c8505554381a8b6efcbe7c689a0e16c
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-03-15 19:38:11 +0100

    Made it a react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+      <input type="text" name="summoner-name" id="summoner-name" />
+      <input type="text" name="summoner-tag" id="summoner-tag" />
+
+    </div>
+  )
+}
+
+export default App
