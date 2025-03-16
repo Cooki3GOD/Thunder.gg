bb89613f6ba5fa4040f53e9f4fab0afcd5acf081
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-03-16 18:41:17 +0100

    Add Region selection

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ const App = () => {
   const [summonerTag, setSummonerTag] = useState("");
   const [summonerData, setSummonerData] = useState(null);
   const [error, setError] = useState(null);
+  const regions = [
+    'br1.api.riotgames.com',
+    'eun1.api.riotgames.com',
+    'euw1.api.riotgames.com',
+    'jp1.api.riotgames.com',
+    'kr.api.riotgames.com', 
+    'la1.api.riotgames.com',
+    'la2.api.riotgames.com',
+    'na1.api.riotgames.com',
+    'oc1.api.riotgames.com',
+    'tr1.api.riotgames.com',
+    'ru.api.riotgames.com',
+    'ph2.api.riotgames.com',
+    'sg2.api.riotgames.com',
+    'th2.api.riotgames.com',
+    'tw2.api.riotgames.com',
+    'vn2.api.riotgames.com',
+  ]
 
   const fetchSummonerData = async () => {
     if (!summonerName || !summonerTag) {
@@ -41,27 +59,55 @@ const App = () => {
   };
 
   return (
-    <div className="container">
-      <div className="summonerSearch">
-        <h2>Thunder<span>.GG</span></h2>
-        <input
-          type="text"
-          placeholder="Game Name (Summoner Name)"
-          value={summonerName}
-          onChange={(e) => setSummonerName(e.target.value)}
-          id="summonerName"
-        />
+    <div className="container w-screen">
+      <div className="summonerSearch flex flex-col justify-center w">
+        <h2 className=" text-center">Thunder<span>.GG</span></h2>
+        <div className="inputContainer text-center flex justify-center">
         
-        <input
-          type="text"
-          placeholder="Enter Summoner Tag"
-          value={summonerTag}
-          onChange={(e) => setSummonerTag(e.target.value)}
-          id="summonerTag"
-        />
-        <button onClick={fetchSummonerData}>.GG</button>
+          <select name="region" id="region" className=" ml-3 mr-2 p-3">
+            
+            <option value="br1">BR</option>
+            <option value="eun1">EUNE</option>
+            <option value="euw1">EUW</option>
+            <option value="jp1">JP</option>
+            <option value="kr">KR</option>
+            <option value="la1">LAN</option>
+            <option value="la2">LAS</option>
+            <option value="na1">NA</option>
+            <option value="oc1">OCE</option>
+            <option value="tr1">TR</option>
+            <option value="ru">RU</option>
+            <option value="ph2">PH</option>
+            <option value="sg2">SG</option>
+            <option value="th2">TH</option>
+            <option value="tw2">TW</option>
+            <option value="vn2">VN</option>
 
-        {error && <p style={{ color: "red" }} id="erorBox">{error}</p>}
+          </select>
+
+          <input
+            type="text"
+            placeholder="Game Name (Summoner Name)"
+            value={summonerName}
+            onChange={(e) => setSummonerName(e.target.value)}
+            id="summonerName"
+            className="mr-2 p-3"
+          />
+          
+          <input
+            type="text"
+            placeholder="Enter Summoner Tag"
+            value={summonerTag}
+            onChange={(e) => setSummonerTag(e.target.value)}
+            id="summonerTag"
+            className=" p-3"
+          />
+
+
+          <button class="btn btn-ghost p-3" onClick={fetchSummonerData}>.GG</button>
+        </div>
+
+        {error && <p style={{ color: "red" }} id="errorBox">{error}</p>}
       </div>
 
       {summonerData && (
